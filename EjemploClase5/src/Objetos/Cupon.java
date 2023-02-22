d09fe8ee480fd2099b8ecafe2d9917cb9ae3b648
Salvador <2054888970310@ingenieria.usac.edu.gt>
2023-02-22 09:07:10 -0600

    Inicio del ejemplo clase 5

---
+++ EjemploClase5/src/Objetos/Cupon.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author lobje
+ */
+public class Cupon {
+    private String codigo;
+    private double porcentaje;
+    /*Varible para verificar si ya se uso*/
+    private boolean disponible;
+    
+    public Cupon(String codigo, double porcentaje)
+    {
+        this.codigo = codigo;
+        this.porcentaje = porcentaje;
+        this.disponible = true;
+    }
+    
+    public String getCodigo()
+    {
+        return this.codigo;
+    }
+    
+    public double getPorcentaje()
+    {
+        return this.porcentaje;
+    }
+    
+    public void setCodigo(String codigo)
+    {
+        this.codigo = codigo;
+    }
+    
+    public void setPorcentaje(double porcentaje)
+    {
+        this.porcentaje = porcentaje;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void noDisponible() {
+        this.disponible = false;
+    }
+}
---
+++ EjemploClase5/src/Objetos/ListaCupones.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lobje
+ */
+public class ListaCupones {
+    private ArrayList<Cupon> listCupon;
+
+    public ListaCupones() {
+        this.listCupon = new ArrayList<>();
+    }
+    
+    public int registrarCupon(String codigo, double porcentaje)
+    {
+        int indice = existCupon(codigo);
+        if(indice!=-1) return 2;
+        if(!cantidadCaracteres(codigo)) return 3;
+        if(!rangoOk(porcentaje)) return 4;
+        
+        //Cupon a = new Cupon(codigo, porcentaje);
+        this.listCupon.add(new Cupon(codigo, porcentaje));
+        return 1;
+    }
+    
+    public int modificarCupo()
+    {
+        return 1;
+    }
+    
+    public void cuponUsados()
+    {
+        
+    }
+    
+    /*El caracteres que sea exactamente 4*/
+    private boolean cantidadCaracteres(String codigo)
+    {
+        if(codigo.length()==4) return true;
+        return false;
+    }
+    
+    /*El rango este 1-99*/
+    private boolean rangoOk(double porcentaje)
+    {
+        if(porcentaje>0 && porcentaje<100) return true;
+        return false;
+    }
+    
+    /*Recorre el array buscando el codigo*/
+    public int existCupon(String codigo)
+    {
+        int cantidad = this.listCupon.size();
+        Cupon tmp;
+        
+        for (int i = 0; i <cantidad ; i++) {
+            tmp = this.listCupon.get(i);
+            if(tmp.getCodigo().equals(codigo)) return i;
+        }
+        return -1;
+    }
+}
