3e89fc0a754875258d8da3201e2d7fc487afd7de
Salvador <2054888970310@ingenieria.usac.edu.gt>
2023-03-15 07:52:58 -0600

    Revert "Comentario"
    This reverts commit 9251218015c6535df198094f59e995edb9c95ab6.

--- EjemploClase8/src/ejemploclase8/EjemploClase8.java
+++ EjemploClase8/src/ejemploclase8/EjemploClase8.java
@@ -15,7 +15,6 @@ public class EjemploClase8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("omentario");
     }
     
 }
