6700408c1d59046ad2ec4f43e16d14fb052f99f4
Salvador <2054888970310@ingenieria.usac.edu.gt>
2023-04-21 18:33:07 -0600

    Se agrego ejemplo de la clase 12

--- Ejemplo1Clase12/src/ejemplo1clase12/Ejemplo1Clase12.java
+++ Ejemplo1Clase12/src/ejemplo1clase12/Ejemplo1Clase12.java
@@ -2,32 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejemploclase12;
+package ejemplo1clase12;
 
-import Grafico.VentanaInicio;
-import Listas.Imagen;
-import Listas.ListaImagen;
+import ListasEnlazadas.ImagenP;
+import ListasEnlazadas.ListaImagen;
 
 /**
  *
  * @author lobje
  */
-public class EjemploClase12 {
+public class Ejemplo1Clase12 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        VentanaInicio vi = new VentanaInicio();
-        vi.setVisible(true);
-        
         ListaImagen li = new ListaImagen();
-        
-        li.add(new Imagen("Primero",""));
-        li.add(new Imagen("Primero",""));
-        li.mostrar();
-        
+        li.add(new ImagenP("Hola", "Hola"));
+        li.delete("hola");
+        li.delete("Hola");
+        li.update("Hola", "Adios");
     }
     
 }
